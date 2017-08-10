25b9643bb9fcab9a06374b17a273793e0a218452
Daniel Bradley <daniel.bradley@o2.com>
2017-08-10 10:03:53 +0100

    Use unzip instead of tar
    - Update the readme to be a little clearer.
    - Add required capability for CF deploy.

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
-## Running locally
+## What is it
+
+An attempt to follow best practices to create a production-ready installation of Sentry.io on AWS.
+
+## Getting Started
+
+1. Upload `cloud-formation.yaml` to the [AWS console](https://console.aws.amazon.com/cloudformation/home?#/stacks/new)
+2. Fill in the parameters.
+3. Review, and click "Create".
+
+### Automating Deployments
+
+This is a great option if you want to automatically deploy your stack from your CI server:
+
+1. Take a copy of `deploy-example.sh`.
+2. Fill in the parameters.
+
+### Running locally
 
 ```
 vagrant up
 ansible-playbook site.yml -u vagrant
 ```
 
-## Deploying to AWS CloudFormation
+Your site should then be available at https://33.33.33.20/
 
-You can either use the AWS console, uploading from your local machine.
+## Credits
 
-Or, use the `deploy-example.sh` to automate deployment (replacing with your own values).
+Original version developed by Karl Turner (@otaiga), Bradley Allen (@ValkyrieUK) and Daniel Bradley (@danielrbradley).
+
+[Using AWS KMS to Encrypt Values in CloudFormation Stacks](https://ben.fogbutter.com/2016/02/22/using-kms-to-encrypt-cloud-formation-values.html) by Ben Jones (@RealSalmon)
+
+Starting point for CloudFormation setup: https://github.com/acervos/sentry
--- deploy-example.sh
+++ deploy-example.sh
@@ -22,8 +22,8 @@ aws cloudformation describe-stacks --stack-name "$STACK_NAME"&>/dev/null
 if [ $? -eq 0 ]
 then
   echo "Updating existing stack"
-  aws cloudformation update-stack --stack-name "$STACK_NAME" --template-body "$TEMPLATE_PATH" --parameters "$OWNER" "$SENTRY_USER" "$SENTRY_PASSWORD" "$DNS_NAME" "$SSL_CERT_ARN" "$SSH_KEY_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$SENTRY_SECRET_KEY" "$GITHUB_APP_ID" "$GITHUB_API_SECRET" "$MAIL_USERNAME" "$MAIL_PASSWORD" "$MAIL_FROM"
+  aws cloudformation update-stack --stack-name "$STACK_NAME" --template-body "$TEMPLATE_PATH" --parameters "$OWNER" "$SENTRY_USER" "$SENTRY_PASSWORD" "$DNS_NAME" "$SSL_CERT_ARN" "$SSH_KEY_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$SENTRY_SECRET_KEY" "$GITHUB_APP_ID" "$GITHUB_API_SECRET" "$MAIL_USERNAME" "$MAIL_PASSWORD" "$MAIL_FROM" --capabilities CAPABILITY_IAM
 else
   echo "Creating new stack"
-  aws cloudformation create-stack --stack-name "$STACK_NAME" --template-body "$TEMPLATE_PATH" --parameters "$OWNER" "$SENTRY_USER" "$SENTRY_PASSWORD" "$DNS_NAME" "$SSL_CERT_ARN" "$SSH_KEY_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$SENTRY_SECRET_KEY" "$GITHUB_APP_ID" "$GITHUB_API_SECRET" "$MAIL_USERNAME" "$MAIL_PASSWORD" "$MAIL_FROM"
+  aws cloudformation create-stack --stack-name "$STACK_NAME" --template-body "$TEMPLATE_PATH" --parameters "$OWNER" "$SENTRY_USER" "$SENTRY_PASSWORD" "$DNS_NAME" "$SSL_CERT_ARN" "$SSH_KEY_NAME" "$DB_USERNAME" "$DB_PASSWORD" "$SENTRY_SECRET_KEY" "$GITHUB_APP_ID" "$GITHUB_API_SECRET" "$MAIL_USERNAME" "$MAIL_PASSWORD" "$MAIL_FROM" --capabilities CAPABILITY_IAM
 fi
