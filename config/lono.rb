b629be862f65f8d4780cc388f2626f8160f3be12
Daniel Bradley <daniel.bradley@o2.com>
2017-08-11 10:56:04 +0100

    Add non-internet-facing variation of templates
    - TODO: The NAT gateway refers to the public subnet groups ... what do we need to change here?

--- config/lono.rb
+++ config/lono.rb
@@ -24,3 +24,30 @@ template "sentry-formation-public-3az.yaml" do
     :availability_zones => 3,
   )
 end
+
+template "sentry-formation-internal-1az.yaml" do
+  source "sentry-formation.yaml.erb"
+  variables(
+    :Description => "Sentry.io internal setup in only 1 availability zone",
+    :visibility => "internal",
+    :availability_zones => 1,
+  )
+end
+
+template "sentry-formation-internal-2az.yaml" do
+  source "sentry-formation.yaml.erb"
+  variables(
+    :Description => "Sentry.io internal setup across 2 availability zones",
+    :visibility => "internal",
+    :availability_zones => 2,
+  )
+end
+
+template "sentry-formation-internal-3az.yaml" do
+  source "sentry-formation.yaml.erb"
+  variables(
+    :Description => "Sentry.io internal setup across 3 availability zones",
+    :visibility => "internal",
+    :availability_zones => 3,
+  )
+end
